f0cc58205caad3ab78ac6c3ce931e0e26dac5b0a
Jeremy Galloway <JeremyNGalloway@gmail.com>
2015-05-17 18:15:27 -0500

    dumps info for windows systems
    initial push // public version w/o OPSEC

---
+++ postpwn.bat
@@ -0,0 +1,27 @@
+del C:\pp.out
+hostname >> C:\pp.out
+whoami >> C:\pp.out
+net users >> C:\pp.out
+net localgroup >> C:\pp.out
+net localgroup administrators >> C:\pp.out
+ipconfig/all >> C:\pp.out
+route print >> C:\pp.out
+C:
+cd C:\
+dir /s *unattend.txt* >> C:\pp.out
+dir /s *unattend.xml* >> C:\pp.out
+dir /s *unattended.xml* >> C:\pp.out
+dir /s *sysprep.inf* >> C:\pp.out
+dir /s *proof* >> C:\pp.out
+dir /s *secret* >> C:\pp.out
+dir /s *.rar* >> C:\pp.out
+systeminfo >> C:\pp.out
+wmic product get name >> C:\pp.out
+set >> C:\pp.out
+netstat -an >> C:\pp.out
+tasklist /V >> C:\pp.out
+dir C:\ >> C:\pp.out
+tree /f /a C:\Users >> C:\pp.out
+tree /f /a  C:\"Documents and Settings">> C:\pp.out
+echo "f1n" >> C:\pp.out
+type C:\pp.out
